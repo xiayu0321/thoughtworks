e6388698ccb8dbd8b638166ba66952be2fd1d723
xiayu0321 <xiayugogo0321@163.com>
2016-06-04 10:45:36 +0800

    task3

---
+++ task3/src/calculate-median.js
@@ -0,0 +1,46 @@
+function calculate_median(arr) {
+  //请勿改动函数名
+  var i = 0;
+  var j = 0;
+  var len = 0;
+  var max;
+  var min;
+  var result = 0;
+  var newArr = new Array();
+  for(i = 0;i < arr.length;i++){
+  	if((i + 1) % 2 == 0){
+  		newArr[j++] = arr[i];
+  	}
+  }  
+  len = newArr.length;
+
+  for (var i = 1; i <= len / 2; i++) {
+     min = 1;
+     max = 1;
+     for (j = j+1; j <= len - i; j++) {
+       if(newArr[j] > newArr[max]){
+          max = j;
+       }
+       if(newArr[j] < newArr[min]){
+            min = j;
+       }
+     }
+     var tmp = newArr[i - 1];
+     newArr[i-1] = newArr[min];
+     newArr[min] = tmp;  
+    
+     tmp = newArr[n-i]; 
+     newArr[n-i] = newArr[max]; 
+     newArr[max] = tmp;   
+  }
+
+  if(len % 2 == 0){
+  	result = newArr[len / 2] + newArr[len / 2  + 1];
+  }else{
+  	result = newArr[len / 2 + 1];
+  }
+}
+　　return result;
+}
+
+module.exports = calculate_median;
