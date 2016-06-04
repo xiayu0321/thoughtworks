c42321a91ad310bb419973079fdb1cd0ea7f8023
xiayu0321 <xiayugogo0321@163.com>
2016-06-04 14:39:47 +0800

    task3

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,17 +1,17 @@
 function calculate_median(arr) {
   //请勿改动函数名
   var i = 0;
-  var len = 0;
+  var j = 0;
   var newArr = [];
   for(i = 0;i < arr.length;i++){
   	if((i + 1) % 2 === 0){
-  	len = newArr.push(arr[i]);   
+  	    newArr[j++] = arr[i];   
   	}
   }  
   newArr.sort(function(a,b){
     return a>b?1:-1});
 
-   var result = (len % 2 === 0 ?newArr[len / 2] + newArr[len / 2  - 1]: newArr[len / 2]);
+   var result = ((j - 1)% 2 === 0 ?newArr[j / 2] + newArr[j / 2  - 1]: newArr[j / 2]);
 
    return result;
 }
